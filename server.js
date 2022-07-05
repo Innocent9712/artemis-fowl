1350593694239ddc575db6dc1ddb0615c5403769
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-07-05 22:29:50 +0100

    Edit a bit

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to this Server")
 })
 
-let PORT = process.env.port || 8080
+let PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
